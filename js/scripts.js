881124e18e83cf41b316912874b7c89739f8cace
hannahrosey <hannah.gorman31@gmail.com>
2022-02-17 16:56:29 -0500

    feat: add map of 311 data during Hurricane Ida, explainer text, and image

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+// Get data and load to map
+$.ajax({
+  url: "https://data.cityofnewyork.us/resource/erm2-nwe9.json",
+  type: "GET",
+  data: {
+    "$where" : "created_date>'2021-08-26T00:00:00.000' AND created_date<'2021-09-4T00:00:00.000' AND agency='HPD' AND descriptor='HEAVY FLOW'",
+    "$limit" : "500",
+    "$$app_token": "NOveDvU9PH6eFY3kHQW7kyWtT"
+  }
+}).done(function(leaksData) {
+  console.log(leaksData);
+
+  mapboxgl.accessToken = "pk.eyJ1IjoiaGFubmFocm9zZXkiLCJhIjoiY2t6aG5ocmh0NDNvdzJvbmZxMG44czVyayJ9.GfvFPfGH_vdPxHOIGVGEPg"
+
+  // Display map
+  var map = new mapboxgl.Map({
+    container: 'mapContainer', // HTML container id
+    style: 'mapbox://styles/mapbox/streets-v9', // style URL
+    center: [-73.99,40.70], // starting position as [lng, lat]
+    zoom: 10
+  });
+
+  leaksData.forEach(function(lD) {
+    var popup = new mapboxgl.Popup({offest : 40})
+    .setHTML(`
+      <p>Possible flood at ${lD.incident_address} in ${lD.borough} reported at ${lD.created_date}<p>
+    `);
+
+  new mapboxgl.Marker({
+    color: 'blue'
+  })
+    .setLngLat([lD.location.longitude,lD.location.latitude])
+    .setPopup(popup)
+    .addTo(map);
+  })
+
+  var map = new mapboxgl.Map({
+     container: 'mapContainer', // HTML container id
+     style: 'mapbox://styles/mapbox/dark-v9', // style URL
+     center: wspCenter, // starting position as [lng, lat]
+     zoom: 11,
+   });
+
+});
